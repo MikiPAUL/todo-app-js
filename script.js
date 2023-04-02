4d1ab168249b7dbaed64eeb01fa66832ac15240c
MikiPAUL <mikipaul46@gmail.com>
2023-04-02 23:56:48 +0530

    index.html script.js style.css created

---
+++ script.js
@@ -0,0 +1,8 @@
+const add = document.getElementById("add")
+console.log("Fdasfd");
+add.addEventListener("click", addItem);
+
+const addItem = () => {
+    const list = document.getElementById("itemlist")
+    cosnt =list.append("Hi there");
+}
