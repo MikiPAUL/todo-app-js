aa8c0fd2d3dedb8c7b0d1e62a1eca9108117ba8f
MikiPAUL <mikipaul46@gmail.com>
2023-05-04 00:01:46 +0530

    intial commit

--- script.js
+++
@@ -1,8 +0,0 @@
-const add = document.getElementById("add")
-console.log("Fdasfd");
-add.addEventListener("click", addItem);
-
-const addItem = () => {
-    const list = document.getElementById("itemlist")
-    cosnt =list.append("Hi there");
-}
